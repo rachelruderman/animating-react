637be97f6781a00bb245531b15611ce5bc9dc586
Rachel Ruderman <rachel@yembo.ai>
2019-08-11 18:52:42 -0700

    react-spring custom config playground

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import {useSpring, animated} from 'react-spring';
+import {useSpring, animated, config} from 'react-spring';
 
 const Checkout = ({isOpen}) => {
 
-    const {x}   = useSpring({x: isOpen ? 0 : 100});
+    const {x}   = useSpring({
+        x: isOpen ? 0 : 100,
+        config: config.wobbly
+    });
 
     const styleLeft     = {transform: x.interpolate(x => `translate3d(${x * -1}%, 0, 0)`)};
     const styleRight    = {transform: x.interpolate(x => `translate3d(${x}%, 0, 0)`)};
 
     //awesome solution to pointer events on wrappers
-    const styleWrapper  = {pointerEvents: isOpen? 'all' : 'none'};
+    const styleWrapper  = {pointerEvents: isOpen ? 'all' : 'none'};
 
     return (
         <div className='checkout' style={styleWrapper}>
