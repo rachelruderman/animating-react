5b77e5aacf91746218413226d3d220bb4e69ec27
Rachel Ruderman <rachel@yembo.ai>
2019-08-11 20:03:18 -0700

    Gestures: drag only along x axis

--- src/Gesture.js
+++ src/Gesture.js
@@ -3,16 +3,16 @@ import {useGesture} from 'react-with-gesture';
 import {animated, useSpring} from 'react-spring';
 
 const Gesture = () => {
-    const [{xy}, set] = useSpring( () => ({xy: [0, 0]}))
+    const [{x}, set] = useSpring( () => ({x: 0}) )
 
     const bind = useGesture( ({down, delta}) => {
-        set({xy: down ? delta : [0, 0]})
+        set({x: down ? delta[0] : 0})
     })
 
     return (
         <animated.div 
             style={{
-                transform: xy.interpolate( (x, y) => `translate3d(${x}px,${y}px,0)`)
+                transform: x.interpolate( (x) => `translate3d(${x}px,0,0)`)
             }}
             {...bind()} 
             className='box'/>
