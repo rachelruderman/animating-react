7e2930934570f8da8719498c777c921b37bdf95f
Rachel Ruderman <rachel@yembo.ai>
2019-08-11 18:01:43 -0700

    Physics-based modal animation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, {useState} from 'react';
 import { useSpring, animated } from 'react-spring';
 import logo from './logo.svg';
 import './App.css';
-import Routes from './Routes';
 import Nav from './Nav';
 import Checkout from './Checkout';
 import Modal from './Modal';
 
 const App = () => {
     const [isNavOpen, setNavOpen] = useState(false);
-
+    
     const navAnimation = useSpring({transform: (isNavOpen) ? 
                           `translate3d(0,0,0) scale(1)` : 
                           `translate3d(100%,0,0) scale(0)`});
@@ -31,7 +30,6 @@ const App = () => {
           <Nav style={navAnimation}/>
         </header>
         <main>
-          <Routes/>
           <Modal/>
           <Checkout lisOpen={isNavOpen}/>
         </main>
--- src/Modal.js
+++ src/Modal.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {animated, useTransition} from 'react-spring';
 
-const Modal = () => {
+const Modal = ({closeModal, animation}) => {
     return (
-        <div>
-            <h1>Modal</h1>
+        <div className='modal'>
+            <animated.div className='modal-card' style={animation}>
+                <h1>Modal</h1>
+                <button onClick={closeModal}>
+                    Close
+                </button>
+            </animated.div>
         </div>
     )
 }
 
-export default Modal
+const ModalWrapper = () => {
+    const [on, toggle] = useState(false);
+    const transition = useTransition(on, null, {
+        from:   {opacity: 0, transform: `translate3d(0, -440px, 0)`},
+        enter:  {opacity: 1, transform: `translate3d(0, 0, 0)`},
+        leave:  {opacity: 0, transform: `translate3d(0, -440px, 0)`}
+    })
 
-const ModalWrapper
+    return (
+        <div>
+            {transition.map(({item, key, props: animation}) => {
+                return (item && <Modal key={key} animation={animation} closeModal={() => toggle(false)}/>)
+            })}
+            <button onClick={() => toggle(!on)}>Toggle</button>
+        </div>
+    )
+}
+
+export default ModalWrapper;
